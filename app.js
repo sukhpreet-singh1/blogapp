408e1a8f3d5b5061e11d039df549a3c86979865d
sukhpreet-singh1 <493_bt19@iiitkalyani.ac.in>
2021-02-09 17:50:44 +0530

    blog web app

--- app.js
+++ app.js
@@ -61,8 +61,10 @@ app.get("/blogs/:blogid",function(req,res){
      });
    });
 });
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port,function(){
+  console.log("server started at port 3000");
 });
